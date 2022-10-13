97b4452059edad173248a81f5d8c98518cb6290a
Kevin Kern <65562996+kkern42@users.noreply.github.com>
2022-10-13 12:43:15 -0400

    adding header

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,48 @@
 <script>
   import PSPPLogo from './assets/pingpong.svg'
   import Counter from './lib/Counter.svelte'
+  import Header from './lib/header.svelte'
 </script>
 
 <!-- add in player profiles when db is activated  -->
 
 <main>
-  <div>
+  <Header/>
+  <div class="app">
+    <div>
 
-    <a href="https://brackethq.com/b/k4n6/" target="_blank"> 
-      <img src={PSPPLogo} class="logo" alt="PS PP Logo" />
-    </a>
+      <a href="https://brackethq.com/b/k4n6/" target="_blank"> 
+        <img src={PSPPLogo} class="logo" alt="PS PP Logo" />
+      </a>
+    </div>
+    <h1>PS PING PONG Tournament</h1>
+  
+    <!-- <div class="card">
+      <Counter />
+    </div> -->
+  
+    <p>
+      Check out <a href="https://teams.microsoft.com/l/team/19%3aCsAUOEEmMq-4OoZugqYP4h2HWJ051bI48z3DbSB7Ha81%40thread.tacv2/conversations?groupId=3e8b7471-35de-45b0-9a0b-b40cf83d6d83&tenantId=d52c9ea1-7c21-47b1-82a3-33a74b1f74b8" target="_blank">PS PP NYC</a>, the official Teams channel for NYC PING PONG!
+    </p>
+  
+    <p class="read-the-docs">
+      Reach out to Ashely Kim, JD Pablo or Kevin Kern to learn more
+    </p>
+  
+    <!-- style="width: 150%;" -->
+    <div class="bracket">
+      <h2 class="title">Tier 1 Bracket - Fall 2022</h2>
+      <iframe title="Tier 1 PS PP Tournament" src="https://brackethq.com/b/k4n6/embed/" width="100%" height="550" frameborder="0"></iframe>
+    </div>
+  
+    <hr/>
+  
+    <div class="bracket">
+      <h2 class="title">Tier 2 Bracket - Fall 2022</h2>
+      <iframe title="Tier 2 PS PP Tournament" src="https://brackethq.com/b/x6n6/embed/" width="100%" height="700" frameborder="0"></iframe>
+    </div>
   </div>
-  <h1>PS PING PONG Tournament</h1>
-
-  <!-- <div class="card">
-    <Counter />
-  </div> -->
-
-  <p>
-    Check out <a href="https://teams.microsoft.com/l/team/19%3aCsAUOEEmMq-4OoZugqYP4h2HWJ051bI48z3DbSB7Ha81%40thread.tacv2/conversations?groupId=3e8b7471-35de-45b0-9a0b-b40cf83d6d83&tenantId=d52c9ea1-7c21-47b1-82a3-33a74b1f74b8" target="_blank">PS PP NYC</a>, the official Teams channel for NYC PING PONG!
-  </p>
-
-  <p class="read-the-docs">
-    Reach out to Ashely Kim, JD Pablo or Kevin Kern to learn more
-  </p>
-
-  <!-- style="width: 150%;" -->
-  <div class="bracket">
-    <h2 class="title">Tier 1 Bracket - Fall 2022</h2>
-    <iframe title="Tier 1 PS PP Tournament" src="https://brackethq.com/b/k4n6/embed/" width="100%" height="550" frameborder="0"></iframe>
-  </div>
-
-  <div class="bracket">
-    <h2 class="title">Tier 2 Bracket - Fall 2022</h2>
-    <iframe title="Tier 2 PS PP Tournament" src="https://brackethq.com/b/x6n6/embed/" width="100%" height="700" frameborder="0"></iframe>
-  </div>
+  
 </main>
 
 <style>
---
+++ src/lib/header.svelte
@@ -0,0 +1,43 @@
+<div class="navbar">
+    <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+    <a href="#"><i class="fa fa-fw fa-search"></i>Champions</a> 
+    <a href="#"><i class="fa fa-fw fa-envelope"></i>Past Brackets</a> 
+    <a href="#"><i class="fa fa-fw fa-user"></i>Player Profiles</a>
+  </div>
+
+<style>
+    .navbar {
+  width: 100%;
+  background-color: #333;
+  overflow: auto;
+  position: fixed;
+  height: 4.5rem;
+}
+
+.navbar a {
+
+  float: left;
+  padding: 1.5rem;
+  color: white;
+  text-decoration: none;
+  font-size: 1.1rem;
+}
+
+.navbar a:hover {
+  background-color: #000;
+}
+
+.active {
+  background-color: #FE414D;
+  
+  height: 5.5rem;
+  
+}
+
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+  }
+}
+</style>
